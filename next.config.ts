0354a5630435de139a0e2a2fce295e5f5536abad
yonghuikim <notido@fonetech.co.kr>
2025-12-10 15:33:52 +0900

    refactor. webpack 설정 제거 및 dev 스크립트 수정

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  webpack: config => {
-    config.resolve.alias["fone-design-system_v1"] = path.resolve(
-      "C:/Users/letit/OneDrive/문서/project/fone-design-system/src/index.ts",
-    );
-    return config;
-  },
 };
 
 export default nextConfig;
